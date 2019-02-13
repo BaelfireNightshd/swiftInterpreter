075dffd135fffea267a1dc2300e49a858c021205
BaelfireNightshd <BaelfireNightshd@users.noreply.github.com>
2019-02-12 23:37:29 -0800

    Wrote unit tests for OCTAL-LITERAL

--- tests.py
+++ tests.py
@@ -21,6 +21,9 @@ class TestLexerWhitespace(unittest.TestCase):
         self.assertEqual(t.name, "LINE-BREAK")
         self.assertEqual(t.value, "\u000d\u000a")
 
+
+
+
 class TestLexerIntegerLiteral(unittest.TestCase):
 
     def test_binary_literal(self):
@@ -63,6 +66,110 @@ class TestLexerIntegerLiteral(unittest.TestCase):
         t = stream.next()
         self.assertEqual(t.name, "BINARY-LITERAL")
         self.assertEqual(t.value, "0b1_0______")
+
+
+
+
+    def test_octal_literal(self):
+        stream = lexer.lex("0o")
+        t = stream.next()
+        self.assertNotEqual(t.name, "OCTAL-LITERAL")
+        self.assertNotEqual(t.value, "0o")
+
+        stream = lexer.lex("0o_")
+        t = stream.next()
+        self.assertNotEqual(t.name, "OCTAL-LITERAL")
+        self.assertNotEqual(t.value, "0o_")
+
+        stream = lexer.lex("0o0")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o0")
+
+        stream = lexer.lex("0o1")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o1")
+
+        stream = lexer.lex("0o2")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o2")
+
+        stream = lexer.lex("0o3")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o3")
+
+        stream = lexer.lex("0o4")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o4")
+
+        stream = lexer.lex("0o5")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o5")
+
+        stream = lexer.lex("0o6")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o6")
+
+        stream = lexer.lex("0o7")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o7")
+
+        stream = lexer.lex("0o8")
+        t = stream.next()
+        self.assertNotEqual(t.name, "OCTAL-LITERAL")
+        self.assertNotEqual(t.value, "0o8")
+
+        stream = lexer.lex("0o0__________")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o0__________")
+
+        stream = lexer.lex("0o00000000000")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o00000000000")
+
+        stream = lexer.lex("0o11111111111")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o11111111111")
+
+        stream = lexer.lex("0o22222222222")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o22222222222")
+
+        stream = lexer.lex("0o33333333333")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o33333333333")
+
+        stream = lexer.lex("0o44444444444")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o44444444444")
+
+        stream = lexer.lex("0o55555555555")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o55555555555")
+
+        stream = lexer.lex("0o66666666666")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o66666666666")
+
+        stream = lexer.lex("0o77777777777")
+        t = stream.next()
+        self.assertEqual(t.name, "OCTAL-LITERAL")
+        self.assertEqual(t.value, "0o77777777777")
 
 if __name__ == '__main__':
     unittest.main()
