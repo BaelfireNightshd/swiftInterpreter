96b9c63a0c49e58c34a9c15bb7d8b822e306570b
BaelfireNightshd <BaelfireNightshd@users.noreply.github.com>
2019-03-09 17:11:58 -0800

    Add IDENTIFIER to the lexer.

--- lexer.py
+++ lexer.py
@@ -26,9 +26,33 @@ lg.add("PLUS", r"\+")
 lg.add("MINUS", r"-")
 lg.add("UNDERSCORE", r"_")
 lg.add("PERIOD", r"\.")
-lg.add("FLOATING-POINT-E", r"[eE]")
-lg.add("FLOATING-POINT-P", r"[pP]")
 
+# constructing the identifier grammar
+
+regex_identifier_head_options = r"a-zA-Z"
+regex_identifier_head_options += r"_"
+regex_identifier_head_options += r"\u00A8\u00AA\u00AD\u00AF\u00B2-\u00B5\u00B7-\u00BA"
+regex_identifier_head_options += r"\u00BC-\u00BE\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u00FF"
+regex_identifier_head_options += r"\u0100-\u02FF\u0370-\u167F\u1681-\u181D\u180F-\u1DBF"
+regex_identifier_head_options += r"\u1E00-\u1FFF"
+regex_identifier_head_options += r"\u200B-\u200D\u202A-\u202E\u203F-\u2040\u2054\u2060-\u206F"
+regex_identifier_head_options += r"\u2070-\u20CF\u2100-\u218F\u2460-\u24FF\u2776-\u2793"
+regex_identifier_head_options += r"\u2C00-\u2DFF\u2E80-\u2FFF"
+regex_identifier_head_options += r"\u3004-\u3007\u3021-\u302F\u3031-\u303F\u3040-\uD7FF"
+regex_identifier_head_options += r"\uF900-\uFD3D\uFD40-\uFDCF\uFDF0-\uFE1F\uFE30-\uFE44"
+regex_identifier_head_options += r"\uFE47-\uFFFD"
+regex_identifier_head_options += r"\U00010000-\U0001FFFD\U00020000-\U0002FFFD\U00030000-\U0003FFFD\U00040000-\U0004FFFD"
+regex_identifier_head_options += r"\U00050000-\U0005FFFD\U00060000-\U0006FFFD\U00070000-\U0007FFFD\U00080000-\U0008FFFD"
+regex_identifier_head_options += r"\U00090000-\U0009FFFD\U000A0000-\U000AFFFD\U000B0000-\U000BFFFD\U000C0000-\U000CFFFD"
+regex_identifier_head_options += r"\U000D0000-\U000DFFFD\U000E0000-\U000EFFFD"
+
+regex_identifier_character_options = r"0-9"
+regex_identifier_character_options += r"\u0300-\u036F\u1DC0-\u1DFF\u20D0-\u20FF\uFE20-\uFE2F"
+regex_identifier_character_options += regex_identifier_head_options
+
+regex_identifier_partial_string = "[" + regex_identifier_head_options + "][" + regex_identifier_character_options + "]*"
+regex_identifier_string = regex_identifier_partial_string + "|`" + regex_identifier_partial_string + "`"
+lg.add("IDENTIFIER", regex_identifier_string)
 
 lexer = lg.build()
 
